dfb508bcfa5a4ce296c1c0d1e332396f0b82f423
Kemoy Campbell <kscics@rit.edu>
2022-09-09 09:05:41 -0400

    update the examples with console modification, inputs and variable examples

---
+++ ConsoleVariablesInputs/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// // See https://aka.ms/new-console-template for more information
+Console.Title = "Cool Project";
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Hello, World!");
+Console.ResetColor();
+Console.ReadKey(); //pause the console.. press any key to continue
+
+string myName = "Kemoy Campbell";
+
+Console.WriteLine("Hello world, I am {0}", myName);
+Console.WriteLine("Hello world, I am "+ myName);
+
+Console.WriteLine("\n");
+
+Console.Write("Hello World, here is the magic\n");
+
+Console.Write("I am on a new line??????\n");  
+
+Console.Write("Enter your name:");
+string name = Console.ReadLine();
+
+Console.WriteLine("Hello {0} ", name);
+
+Console.Write("How old are you:");
+int age = Convert.ToInt32(Console.ReadLine());
+
+const int ADD_ONE = 1;
+
+
+Console.WriteLine("Whoa, next year you will be {0} ", (age +ADD_ONE));
