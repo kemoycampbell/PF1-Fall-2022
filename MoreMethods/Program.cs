f9a3b0eb84a06ee3f5a866a936813cb7ecfc2df2
Kemoy Campbell <kscics@rit.edu>
2022-10-14 09:59:59 -0400

    string theory and more methods'

---
+++ MoreMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program
+{
+    
+    //main method
+    public static void Main(string[] args)
+    {
+        int sum = Add(5,1);
+        Console.WriteLine(sum);
+        Console.WriteLine(1);
+        Console.WriteLine(true);
+        Console.WriteLine("Hello");
+        Console.WriteLine();
+
+        sum = Add(3,5) + Add(9,0);
+
+        sum = Add(3,Add(5,9));
+        sum = Add(3,5,9);
+
+        Console.WriteLine(Add("5", "6"));
+
+        Console.WriteLine(Program.Add(5,3));
+    }
+
+    public static int Add(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+
+    public static int Add(int number1, int number2, int number3)
+    {
+        return number1 + number2 + number3;
+    }
+
+    public static double Add(double number1, double number2)
+    {
+        return number1 + number2;
+    }
+
+    public static int Add(string num1, string num2)
+    {
+        return int.Parse(num1) + int.Parse(num2);
+    }
+
+}
---
+++ StringTheory/Program.cs
@@ -0,0 +1,62 @@
+﻿string name = "Sierra";
+
+// char[] letters = name.ToCharArray();
+
+// foreach(char letter in letters)
+// {
+//     Console.WriteLine(letter);
+// }
+
+// Console.WriteLine(name);
+
+// Console.WriteLine(name.ToUpper());
+// Console.WriteLine(name.ToLower());
+
+// Console.Write("Enter the password:");
+// string password = Console.ReadLine();
+
+// if(password.ToLower() == "sierra")
+// {
+//     Console.WriteLine("Welcome star!!");
+// }
+
+// while(true)
+// {
+//     Console.Write("Enter a word:");
+//     string word = Console.ReadLine();
+
+//     Console.WriteLine("You type: "+ word);
+
+//     Console.Write("Do you want to contine? (Y/N):");
+//     string answer = Console.ReadLine();
+//     if(answer.ToUpper().ToCharArray()[0] == 'N')
+//     {
+//         break;
+//     }
+// }
+
+
+//searching in a string
+string data = "RIT is an awesome university... well, maybe";
+// while(true)
+// {
+//     Console.Write("What do you want to search for?");
+//     string target = Console.ReadLine();
+
+//     int matchLocation = data.IndexOf(target);
+//     if(matchLocation==-1){
+//         Console.WriteLine("No match found");
+//     } else {
+//         Console.WriteLine("Match at "+ matchLocation);
+//     }
+
+
+// }
+
+string[] words = data.Split(" ");
+
+Console.WriteLine(words.Length);
+
+foreach(string word in words){
+    Console.WriteLine(word);
+}
